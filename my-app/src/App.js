635c2fda8ae333480185effb0b7e637550417f49
Tammie Myers <tammie.myers@gmail.com>
2021-10-13 21:37:30 -0400

    second commit

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './images/marvel_background.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          WIP
+        </p>
+        <a
+          // className="App-link"
+          // href="https://reactjs.org"
+          // target="_blank"
+          // rel="noopener noreferrer"
+        >
+          Marvel Dashboard
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
